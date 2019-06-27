8563d5352ab96c95a6fa7fe00ccf27473b5ddbea
Leandro Facchinetti <me@leafac.com>
2019-06-27 09:39:00 -0400

    Prepare for adding elements

--- index.js
+++ index.js
@@ -3,17 +3,8 @@ const { app, globalShortcut, BrowserWindow } = require("electron");
 app.dock.hide();
 
 app.on("ready", () => {
-  const window = new BrowserWindow({
-    width: 250,
-    height: 400,
-    frame: false,
-    resizable: false,
-    show: false,
-    webPreferences: { nodeIntegration: true }
-  });
+  const window = new BrowserWindow({ width: 250, height: 400, frame: false, resizable: false, show: false, webPreferences: { nodeIntegration: true } });
   window.loadFile("index.html");
-  globalShortcut.register("Control+Option+Command+Space", () => {
-    window.show();
-    window.webContents.executeJavaScript(`select.focus();`);
+  globalShortcut.register("Control+Option+Command+Space", () => { window.show(); window.webContents.executeJavaScript(`select.focus();`);
   });
 });
