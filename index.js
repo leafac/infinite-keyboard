44d81a5d5381a7fad476dd10056da1c926300778
Leandro Facchinetti <me@leafac.com>
2019-06-27 02:02:45 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { app, globalShortcut, BrowserWindow } = require("electron");
+
+app.dock.hide();
+
+app.on("ready", () => {
+  const window = new BrowserWindow({
+    width: 250,
+    height: 400,
+    frame: false,
+    resizable: false,
+    show: false,
+    webPreferences: { nodeIntegration: true }
+  });
+  window.loadFile("index.html");
+  globalShortcut.register("Control+Option+Command+Space", () => {
+    window.show();
+    window.webContents.executeJavaScript(`select.focus();`);
+  });
+});
