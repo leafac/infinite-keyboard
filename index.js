ee31540faf0b816098a09cbad225b6eae547bfd0
Leandro Facchinetti <me@leafac.com>
2019-06-27 14:55:22 -0400

    Close

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const { app, globalShortcut, BrowserWindow } = require("electron");
 app.dock.hide();
 
 app.on("ready", () => {
-  const window = new BrowserWindow({ width: 250, height: 400, frame: false, resizable: false, show: false, webPreferences: { nodeIntegration: true } });
+  const window = new BrowserWindow({
+    width: 250,
+    height: 400,
+    frame: false,
+    resizable: false,
+    show: false,
+    webPreferences: { nodeIntegration: true }
+  });
   window.setVisibleOnAllWorkspaces(true);
   window.loadFile("index.html");
-  globalShortcut.register("Control+Option+Command+Space", () => { window.show(); window.webContents.executeJavaScript(`select.focus();`);
+  globalShortcut.register("Control+Option+Command+Space", () => {
+    if (window.isVisible()) {
+      window.webContents.executeJavaScript(`select.blur();`);
+    }
+    else {
+      window.webContents.executeJavaScript(`select.focus();`);
+      window.show();
+    }
   });
 });
